b6a09fd5efece40a9625764e355a0c6876656927
Dan Burzo <danburzo@gmail.com>
2018-09-26 11:04:06 +0300

    Draft implementation

---
+++ README.md
@@ -0,0 +1,11 @@
+# tiny-i18n
+
+Tiny (experimental) i18n library for JavaScript using [tagged template literals](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals).
+
+## Further reading 
+
+* [i18n with tagged template literals in ECMAScript 2015](https://jaysoo.ca/2014/03/20/i18n-with-es2015-template-literals/) by Jack Hsu 
+* [Easy i18n in 10 lines of JavaScript (PoC)](https://codeburst.io/easy-i18n-in-10-lines-of-javascript-poc-eb9e5444d71e) by Andrea Giammarchi
+* [Tagged Template literals — It's more than you think](https://codeburst.io/javascript-es6-tagged-template-literals-a45c26e54761) by Bharathvaj Ganesan
+* [i18n Tagged Template Literals](http://i18n-tag.kolmer.net/) by Steffen Kolmer
+
---
+++ index.js
@@ -0,0 +1,41 @@
+const db = {};
+
+/*
+	Combines two arrays of strings of (mostly) the same length
+	into a single string, through interpolation.
+ */
+function interpolate(strings, values) {
+	return strings.reduce(function(result, current_str, idx) {
+		return result + current_str + (idx < values.length ? values[idx] : '');
+	}, '');
+}
+
+function key(strings) {
+	return strings.join('{}');
+}
+
+function value(strings, ...placeholders) {
+	return values => interpolate(strings, placeholders.map(i => values[i]));
+}
+
+function translate(strings, ...values) {
+	let k = key(strings);
+	if (!db[k]) {
+		console.warn(`No translation found for ${k}`);
+		return interpolate(strings, values);
+	}
+	return db[k](values);
+}
+
+function load(k, v) {
+	if (typeof k === 'object') {
+		Object.keys(k).forEach(i => {
+			db[i] = k[i];
+		});
+	}
+	db[k] = v;
+}
+
+export translate as t;
+export value as v;
+export key as k;
