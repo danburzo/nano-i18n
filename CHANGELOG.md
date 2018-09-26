4fe79ce96a6c076ed74e641013993a09430b0df3
Dan Burzo <danburzo@gmail.com>
2018-09-26 12:50:00 +0300

    Annotate source code

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,10 @@
-# 1.0.4
+# Changelog
+
+### 1.0.4
 
 -   Adds a `clear()` method to empty the translations store.
 -   Adds basic tests to the library.
 
-# 1.0.3 and below
+### 1.0.3 and below
 
 Time immemorial.
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/*
+	The translation store.
+	Add things to it with load(),
+	and empty it with clear().
+ */
 let db = {};
 
 /*
@@ -10,10 +15,21 @@ function interpolate(strings, values) {
 	}, '');
 }
 
+/*
+	Generate a key for a certain literal.
+	Interpolated values from the template literal are ignored,
+	and replaced with the `{}` placeholder.
+
+	E.g. key`Hello, ${'World'}` => "Hello, {}"
+ */
 function key(strings) {
 	return strings.join('{}');
 }
 
+/*
+	Provide a translation for a string.
+	Used in conjunction with `key` and `load`.
+ */
 function value(strings, ...placeholders) {
 	return values =>
 		placeholders.length
@@ -21,6 +37,14 @@ function value(strings, ...placeholders) {
 			: strings.join('');
 }
 
+/*
+	Translate a string. E.g.
+
+		t`Hello, ${'World'}!` => "Salut, World!"
+
+	If no translation can be found, 
+	a simple interpolation is returned instead.
+ */
 function translate(strings, ...values) {
 	let k = key(strings);
 	if (!db[k]) {
@@ -30,6 +54,14 @@ function translate(strings, ...values) {
 	return db[k](values);
 }
 
+/*
+	Load translations into the store.
+
+	You can either load them in bulk by sending
+	an object as the first argument, or one by one:
+
+		load(k`Hello, ${'World'}`, v`Salut, ${0}`);
+ */
 function load(k, v) {
 	if (typeof k === 'object') {
 		Object.keys(k).forEach(i => {
@@ -39,6 +71,9 @@ function load(k, v) {
 	db[k] = v;
 }
 
+/*
+	Empty the translations store.
+ */
 function clear() {
 	db = {};
 }
